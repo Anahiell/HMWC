15de41e3c35effbec411b7da2b59b1662508187c
Бурдейный Евгений <anahielitl@gmail.com>
2023-05-26 23:05:09 +0300

    Music Leabrary

---
+++ Album.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Musick_Album
+{
+    [Serializable]
+    class Album
+    {
+        public string NameAlbum { get; set; }
+        public string NameOfCreator { get; set; }
+        public DateTime DateCreate { get; set; }
+        private int Minut;
+        private int Second;
+        public string FullTime { get; set; }
+        public string Studia { get; set; }
+        public Album()
+        {
+
+        }
+        public Album(string Name1,string Name2,DateTime Date,int Min,int Sec,string Name3)
+        {
+            NameAlbum = Name1;
+            NameOfCreator = Name2;
+            Studia = Name3;
+            DateCreate = Date;
+            Minut = Min;
+            Second = Sec;
+            FullTime = $"{Minut}:{Second}";
+        }
+        public void EnterInfo()
+        {
+            Console.WriteLine("Enter Name of album:");
+            NameAlbum = Console.ReadLine();
+            Console.WriteLine("Enter Name of creator:");
+            NameOfCreator = Console.ReadLine();
+            Console.WriteLine("Enter Date of create:");
+            DateCreate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Full time of album(minuts after seconds):");
+            Minut = int.Parse(Console.ReadLine());
+            Second = int.Parse(Console.ReadLine());
+            FullTime = $"{Minut}:{Second}";
+            Console.WriteLine("Enter Studio:");
+            Studia = Console.ReadLine();
+        }
+        public override string ToString()
+        {
+            return $"Name:{NameAlbum}\nCreator:{NameOfCreator}\nDate:{DateCreate}\n" +
+                $"Full Time:"+FullTime+$"\nStudio:{Studia}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using NLog;
+
+namespace Musick_Album
+{
+   
+    class Program
+    {
+        
+        public static string path = "Albums.dat";
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Album al1 = new Album("Revolver", "The Beatles",new DateTime(1966,8,5), 34, 12, "EMI Studios");
+            Album al2 = new Album("Thriller", "Michael Jackson", new DateTime(1982, 11, 30), 42, 02, "Westlake recording Studios");
+            Album al3 = new Album("Dark Side of the Moon", "Pink Floyd", new DateTime(1973, 3, 1), 43, 0, "Abbey Road Studios");
+
+            LibConw Liberty = new LibConw();
+            Liberty.AddAlbum(al1);
+            Liberty.AddAlbum(al2);
+            Liberty.AddAlbum(al3);
+            Liberty.Show();
+            Liberty.Serialiaz(path);
+            Liberty.Deserialize(path);
+            Liberty.SaveToXml();
+            Liberty.Show();
+        }
+    }
+}
